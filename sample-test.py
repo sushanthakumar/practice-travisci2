deaa3964c63f27fab0a0b65300b63048d9023485
sushanth <sushanth.github@gmail.com>
2024-02-06 09:10:49 +0530

    Create sample-test.py

---
+++ sample-test.py
@@ -0,0 +1,9 @@
+# Test Sum function
+from sample import Sum
+
+def TestSum():
+        assert Sum(2,3,5) == 10
+        print("Sum function verified")
+
+if __name__ == '__main__':
+        TestSum()
